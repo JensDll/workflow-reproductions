d7aa90bbb1021d571b46fbd657435c31a5ba5cb4
jens <jens@doellmann.com>
2022-05-26 02:43:21 +0200

    my commit

--- services/web/index.ts
+++ services/web/index.ts
@@ -12,37 +12,50 @@ const repo = 'workflow-reproductions'
 
 type Options = {
   title: string
+  head: string
+  base: string
+  deleteHead?: boolean
 }
 
 async function pullMerge(options: Options) {
+  const { data: head } = await github.rest.git.getRef({
+    owner,
+    repo,
+    ref: `heads/${options.head}`
+  })
+
   const { data: pullRequest } = await github.rest.pulls.create({
     owner,
     repo,
-    base: 'main',
-    head: 'staging',
+    base: options.base,
+    head: options.head,
     title: options.title,
     body: '',
     commit_message: options.title
   })
 
-  console.log(pullRequest)
+  await github.rest.pulls.merge({
+    owner,
+    repo,
+    pull_number: pullRequest.number,
+    merge_method: 'merge'
+  })
+
+  await github.rest.git.updateRef({
+    owner,
+    repo,
+    ref: `heads/${options.base}`,
+    sha: head.object.sha,
+    force: true
+  })
+
+  if (options.deleteHead) {
+    await github.rest.git.deleteRef({
+      owner,
+      repo,
+      ref: `heads/${options.head}`
+    })
+  }
 }
 
-await pullMerge({ title: crypto.randomUUID() })
-
-// const mergeResult = await github.rest.pulls.merge({
-//   owner: '${{ github.event.repository.owner.login }}',
-//   repo: '${{ github.event.repository.name }}',
-//   pull_number: pullRequest.data.number,
-//   merge_method: '${{ inputs.merge_method }}'
-// })
-
-// console.log(mergeResult.status)
-// if ('${{ inputs.delete_head }}' === 'false') {
-//   return
-// }
-// const deleteResult = await github.rest.git.deleteRef({
-//   owner: '${{ github.event.repository.owner.login }}',
-//   repo: '${{ github.event.repository.name }}',
-//   ref: 'heads/${{ inputs.head }}'
-// })
+await pullMerge({ title: crypto.randomUUID(), base: 'staging', head: 'main' })
