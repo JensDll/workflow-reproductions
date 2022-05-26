258853f406704d23477c95d60a2ce7fbee3478bc
jens <jens@doellmann.com>
2022-05-26 13:01:16 +0200

    c

--- services/web/index.ts
+++ services/web/index.ts
@@ -1,4 +1,4 @@
-import { Octokit } from '@octokit/rest'
+import { Octokit, type RestEndpointMethodTypes } from '@octokit/rest'
 import crypto from 'node:crypto'
 
 const github = new Octokit({
@@ -20,6 +20,62 @@ type Options = {
   merge_method?: MergeMethod
 }
 
+async function checkWorkflowRun(
+  ref: RestEndpointMethodTypes['git']['getRef']['response']['data']
+) {
+  const branch = ref.ref.match(/[^/]+$/)![0]
+
+  const {
+    data: { workflow_runs }
+  } = await github.rest.actions.listWorkflowRunsForRepo({
+    owner,
+    repo,
+    branch,
+    exclude_pull_requests: true,
+    event: 'push',
+    per_page: 10
+  })
+
+  const runs = workflow_runs.filter(run => run.head_sha === ref.object.sha)
+
+  console.log(
+    `Found ${runs.length} workflow run result(s) for commit: ${ref.object.sha}`
+  )
+  console.log('Checking for failures ...')
+
+  let anyFailed = false
+
+  for (const run of runs) {
+    if (run.status === 'failure') {
+      anyFailed = true
+    }
+  }
+
+  if (anyFailed) {
+    throw new Error(
+      'Aborting merge: One or more workflow runs failed for this commit'
+    )
+  }
+
+  // if (lastRun) {
+  //   console.log(`Checking workflow run result for commit '${ref.object.sha}'`)
+
+  //   if (lastRun.head_sha !== ref.object.sha) {
+  //     throw new Error(
+  //       'Aborting merge: Workflow run sha does not match head sha'
+  //     )
+  //   }
+
+  //   if (lastRun.status === 'failure') {
+  //     throw new Error(
+  //       'Aborting merge: Workflow run did not succeed, please check the logs'
+  //     )
+  //   }
+  // }
+
+  // console.log(lastRun)
+}
+
 async function pullMerge(options: Options) {
   options.merge_method ??= 'github_merge'
 
@@ -29,23 +85,31 @@ async function pullMerge(options: Options) {
     ref: `heads/${options.head}`
   })
 
-  const { data: headCommit } = await github.rest.git.getCommit({
-    owner,
-    repo,
-    commit_sha: headRef.object.sha
-  })
+  await checkWorkflowRun(headRef)
 
-  console.log(headCommit)
+  // const { data: headCommit } = await github.rest.git.getCommit({
+  //   owner,
+  //   repo,
+  //   commit_sha: headRef.object.sha
+  // })
 
-  const { data: pullRequest } = await github.rest.pulls.create({
-    owner,
-    repo,
-    base: options.base,
-    head: options.head,
-    title: options.title,
-    body: '',
-    commit_message: options.title
-  })
+  // console.log(headCommit)
+
+  // const { data: pullRequest } = await github.rest.pulls.create({
+  //   owner,
+  //   repo,
+  //   base: options.base,
+  //   head: options.head,
+  //   title: options.title,
+  //   body: '',
+  //   commit_message: options.title
+  // })
+
+  // const { data: commits } = await github.rest.pulls.listCommits({
+  //   owner,
+  //   repo,
+  //   pull_number: pullRequest.number
+  // })
 
   // await github.rest.pulls.merge({
   //   owner,
